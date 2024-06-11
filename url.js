a2fe88fb2a6038aa50d6b024c0b7a648dd577bce
Gongal Reddy Jahnavi <111899374+jahnavireddy-27@users.noreply.github.com>
2024-06-11 17:16:53 +0530

    Update url.js

--- url.js
+++ url.js
@@ -1,3 +1,2 @@
 //module.exports='mongodb://localhost:27017'
-
-module.exports='mongodb+srv://jahnavi:jahnavi@mdb.vtkja.mongodb.net/'
+module.exports='mongodb+srv://jahnavi:jahnavi@cluster0.ykr6sig.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
